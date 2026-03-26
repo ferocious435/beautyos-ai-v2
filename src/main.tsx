679e26a4c06e1100acafd3ea6eb32119e090fd71
Sergey Raihshtat <sergey@hm-ce.co.il>
2026-03-26 09:48:36 +0200

    Stepwise Debug: Restore simplified App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
 
 console.log('CRITICAL: main.tsx is executing');
 const rootElement = document.getElementById('root');
 
-if (rootElement) {
-  rootElement.innerHTML = '<h1 style="color:white;padding:50px;text-align:center;">JS EXECUTED - MOUNTING...</h1>';
-}
-
 createRoot(rootElement!).render(
   <StrictMode>
-    <div style={{ color: 'yellow', fontSize: '30px', textAlign: 'center', marginTop: '100px' }}>
-      REACT IS RENDERING! 🚀
-    </div>
+    <App />
   </StrictMode>
 )
